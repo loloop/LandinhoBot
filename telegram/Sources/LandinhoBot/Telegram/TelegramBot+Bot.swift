6be72cf86fcd51951ac182fa7ded942b648223d6
Mauricio Cardozo <mauricio.cardozo@me.com>
2023-11-16 21:42:12 -0300

    fixes an issue with character escaping in the bot

--- telegram/Sources/LandinhoBot/Telegram/TelegramBot+Bot.swift
+++ telegram/Sources/LandinhoBot/Telegram/TelegramBot+Bot.swift
@@ -9,13 +9,24 @@ import Foundation
 import TelegramBotSDK
 
 extension TelegramBot: Bot {
-  public func reply(_ update: ChatUpdate, text: String) async throws {
+  public func reply(_ update: ChatUpdate, text: String) async throws {  
     guard let chatID = Int64(update.chatID) else {
       throw TelegramError(message: "Couldn't convert chatID String to Int64")
     }
+
+    let escapedText = escapeCharacters(in: text)
     try await sendMessageAsync(
       chatId: .chat(chatID),
-      text: text,
+      text: escapedText,
       parseMode: .markdownv2)
   }
+
+  private func escapeCharacters(in text: String) -> String {
+    // Inefficient but gets the job done
+    let charactersToEscape = ["_", "*", "[", "]", "(", ")", "~", "`", ">", "#", "+", "-", "=", "|", "{", "}", ".", "!"]
+    return charactersToEscape.reduce(text) { partialResult, character in
+      partialResult.replacingOccurrences(of: character, with: #"\"#+character)
+    }
+  }
+
 }
