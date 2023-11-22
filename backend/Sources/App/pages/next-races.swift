423011c88c37873b37eb5f75ea6c55d814362742
Mauricio Cardozo <mauricio.cardozo@me.com>
2023-11-22 01:32:24 -0300

    adds filtering by category tag to next-races

--- backend/Sources/App/pages/next-races.swift
+++ backend/Sources/App/pages/next-races.swift
@@ -14,16 +14,31 @@ struct NextRacesHandler: AsyncRequestHandler {
   var path: String { "next-races" }
 
   func handle(req: Request) async throws -> some AsyncResponseEncodable {
-
     let currentDate = Date()
 
-    return try await Race
+    var categoryTag: String
+    do {
+      categoryTag = try req.query.decode(NextRacesRequest.self).category
+    } catch {
+      categoryTag = ""
+    }
+
+    let query = Race
       .query(on: req.db)
       .filter(\.$earliestEventDate, .greaterThanOrEqual, currentDate)
       .join(parent: \.$category)
       .sort(\.$earliestEventDate, .ascending)
       .with(\.$events)
       .with(\.$category)
-      .paginate(for: req)
+
+    if !categoryTag.isEmpty {
+      query.filter(Category.self, \.$tag, .equal, categoryTag)
+    }
+
+    return try await query.paginate(for: req)
+  }
+
+  struct NextRacesRequest: Content {
+    let category: String
   }
 }
